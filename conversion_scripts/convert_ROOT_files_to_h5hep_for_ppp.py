dd0ba77f23379b01d061dcc0d2e46f52678d9a8b
mattbellis <matthew.bellis@gmail.com>
2018-06-20 02:31:32 -0400

    Added ROOT conversion script.

--- conversion_scripts/convert_ROOT_files_to_h5hep_for_ppp.py
+++ conversion_scripts/convert_ROOT_files_to_h5hep_for_ppp.py
@@ -3,9 +3,16 @@ import sys
 sys.path.append('../pps_tools')
 import h5hep 
 
-import cms_tools as cms
-import babar_tools as babar
-import cleo_tools as cleo
+import ROOT 
+################################################################################
+def sph2cart(pmag,costh,phi):
+    theta = np.arccos(costh)
+    x = pmag*np.sin(theta)*np.cos(phi)
+    y = pmag*np.sin(theta)*np.sin(phi)
+    z = pmag*costh
+    return x,y,z
+################################################################################
+
 
 ################################################################################
 def convert_cms(infilename):
@@ -27,16 +34,18 @@ def convert_cms(infilename):
     
     event = h5hep.create_single_event(data)
 
-    collisions = cms.get_collisions_from_filename(infilename)
+    f = ROOT.TFile(infilename)
+    t = ROOT.Get('ntp1')
+
+    nentries = t.GetEntries()
 
     #'''
-    for collision in collisions:
+    for i in range(nentries):
 
-        h5hep.clear_event(event)
+        t.GetEvent(i)
 
-        jets,muons,electrons,photons,met = collision
-
-        event['jets/njets'] = len(jets)
+        '''
+        event['jets/njets'] = len
         for jet in jets:
             event['jets/e'].append(jet[0])
             event['jets/px'].append(jet[1])
@@ -73,12 +82,14 @@ def convert_cms(infilename):
 
 
         h5hep.pack(data,event)
+        '''
 
+    '''
     print("Writing the file...")
     outfilename = infilename.split('.')[0] + ".hdf5"
     print(outfilename)
     hdfile = h5hep.write_to_file(outfilename,data,comp_type='gzip',comp_opts=9)
-    #'''
+    '''
 ################################################################################
 ################################################################################
 def convert_babar(infilename):
@@ -89,7 +100,7 @@ def convert_babar(infilename):
                ['protons','nprotons',['e','px','py','pz','q','beta','dedx'] ], 
                ['muons','nmuons',['e','px','py','pz','q','beta','dedx'] ], 
                ['electrons','nelectrons',['e','px','py','pz','q','beta','dedx'] ], 
-               ['photons','nphotons',['e','px','py','pz','q','beta','dedx'] ], 
+               ['photons','nphotons',['e','px','py','pz'] ], 
                ]
 
     for group in groups:
@@ -104,35 +115,146 @@ def convert_babar(infilename):
 
     event = h5hep.create_single_event(data)
 
-    collisions = babar.get_collisions(open(infilename))
+    f = ROOT.TFile(infilename)
+    tree = f.Get('ntp1')
 
-    for collision in collisions:
+    nentries = tree.GetEntries()
+
+    #'''
+    for i in range(nentries):
 
         h5hep.clear_event(event)
 
-        pions,kaons,protons,muons,electrons,photons = collision
+        tree.GetEvent(i)
 
-        particles = [pions,kaons,protons,muons,electrons,photons]
+        if i%1000==0:
+            print(i)
 
-        for group,particle in zip(groups, particles):
-            key = "%s/%s" % (group[0],group[1])
-            event[key] = len(particle)
-            for p in particle:
-                for j in range(len(group[2])):
-                    key = '%s/%s' % (group[0],group[2][j])
-                    event[key].append(p[j])
+        #pions,kaons,protons,muons,electrons,photons = collision
+        #particles = [pions,kaons,protons,muons,electrons,photons]
 
-        '''
-        event['pions/npions'] = len(pions)
-        for pion in pions:
-            event['pions/e'].append(pion[0])
-            event['pions/px'].append(pion[1])
-            event['pions/py'].append(pion[2])
-            event['pions/pz'].append(pion[3])
-            event['pions/q'].append(pion[4])
-            event['pions/beta'].append(pion[5])
-            event['pions/dedx'].append(pion[6])
-        '''
+        event['pions/npions'] = tree.npi
+        for j in range(tree.npi):
+            e = tree.pienergy[j]
+            p3 = tree.pip3[j]
+            phi = tree.piphi[j]
+            costh = tree.picosth[j]
+            px,py,pz = sph2cart(p3,costh,phi)
+            lund = tree.piLund[j]
+            q = int(lund/np.abs(lund))
+            idx = tree.piTrkIdx[j]
+            dedx = tree.TRKdedxdch[idx]
+            drc = tree.TRKDrcTh[idx]
+            beta = 1.0/np.cos(drc)/1.474
+
+            event['pions/e'].append(e)
+            event['pions/px'].append(px)
+            event['pions/py'].append(py)
+            event['pions/pz'].append(pz)
+            event['pions/q'].append(q)
+            event['pions/beta'].append(beta)
+            event['pions/dedx'].append(dedx)
+
+        event['kaons/nkaons'] = tree.nK
+        for j in range(tree.nK):
+            e = tree.Kenergy[j]
+            p3 = tree.Kp3[j]
+            phi = tree.Kphi[j]
+            costh = tree.Kcosth[j]
+            px,py,pz = sph2cart(p3,costh,phi)
+            lund = tree.KLund[j]
+            q = int(lund/np.abs(lund))
+            idx = tree.KTrkIdx[j]
+            dedx = tree.TRKdedxdch[idx]
+            drc = tree.TRKDrcTh[idx]
+            beta = 1.0/np.cos(drc)/1.474
+
+            event['kaons/e'].append(e)
+            event['kaons/px'].append(px)
+            event['kaons/py'].append(py)
+            event['kaons/pz'].append(pz)
+            event['kaons/q'].append(q)
+            event['kaons/beta'].append(beta)
+            event['kaons/dedx'].append(dedx)
+
+        event['protons/nprotons'] = tree.np
+        for j in range(tree.np):
+            e = tree.penergy[j]
+            p3 = tree.pp3[j]
+            phi = tree.pphi[j]
+            costh = tree.pcosth[j]
+            px,py,pz = sph2cart(p3,costh,phi)
+            lund = tree.pLund[j]
+            q = int(lund/np.abs(lund))
+            idx = tree.pTrkIdx[j]
+            dedx = tree.TRKdedxdch[idx]
+            drc = tree.TRKDrcTh[idx]
+            beta = 1.0/np.cos(drc)/1.474
+
+            event['protons/e'].append(e)
+            event['protons/px'].append(px)
+            event['protons/py'].append(py)
+            event['protons/pz'].append(pz)
+            event['protons/q'].append(q)
+            event['protons/beta'].append(beta)
+            event['protons/dedx'].append(dedx)
+
+        event['protons/nprotons'] = tree.nmu
+        for j in range(tree.nmu):
+            e = tree.muenergy[j]
+            p3 = tree.mup3[j]
+            phi = tree.muphi[j]
+            costh = tree.mucosth[j]
+            px,py,pz = sph2cart(p3,costh,phi)
+            lund = tree.muLund[j]
+            q = int(lund/np.abs(lund))
+            idx = tree.muTrkIdx[j]
+            dedx = tree.TRKdedxdch[idx]
+            drc = tree.TRKDrcTh[idx]
+            beta = 1.0/np.cos(drc)/1.474
+
+            event['protons/e'].append(e)
+            event['protons/px'].append(px)
+            event['protons/py'].append(py)
+            event['protons/pz'].append(pz)
+            event['protons/q'].append(q)
+            event['protons/beta'].append(beta)
+            event['protons/dedx'].append(dedx)
+
+        event['electrons/nelectrons'] = tree.ne
+        for j in range(tree.ne):
+            e = tree.eenergy[j]
+            p3 = tree.ep3[j]
+            phi = tree.ephi[j]
+            costh = tree.ecosth[j]
+            px,py,pz = sph2cart(p3,costh,phi)
+            lund = tree.eLund[j]
+            q = int(lund/np.abs(lund))
+            idx = tree.eTrkIdx[j]
+            dedx = tree.TRKdedxdch[idx]
+            drc = tree.TRKDrcTh[idx]
+            beta = 1.0/np.cos(drc)/1.474
+
+            event['electrons/e'].append(e)
+            event['electrons/px'].append(px)
+            event['electrons/py'].append(py)
+            event['electrons/pz'].append(pz)
+            event['electrons/q'].append(q)
+            event['electrons/beta'].append(beta)
+            event['electrons/dedx'].append(dedx)
+
+        event['photons/nphotons'] = tree.ngamma
+        for j in range(tree.ngamma):
+            e = tree.gammaenergy[j]
+            p3 = tree.gammap3[j]
+            phi = tree.gammaphi[j]
+            costh = tree.gammacosth[j]
+            px,py,pz = sph2cart(p3,costh,phi)
+
+            event['photons/e'].append(e)
+            event['photons/px'].append(px)
+            event['photons/py'].append(py)
+            event['photons/pz'].append(pz)
 
         h5hep.pack(data,event)
 
@@ -206,6 +328,6 @@ def convert_cleo(infilename):
 if __name__ == "__main__":
     infilename = sys.argv[1]
     #convert_cms(infilename)
-    #convert_babar(infilename)
-    convert_cleo(infilename)
+    convert_babar(infilename)
+    #convert_cleo(infilename)
 
