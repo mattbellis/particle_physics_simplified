8e0021469a7bdbb450b211629da94e2d3bea3024
Dan-Whinnery <33735540+Dan-Whinnery@users.noreply.github.com>
2018-06-26 11:07:42 -0400

    dimuons 1k collisions; BaBar file

--- pps_tools/download.py
+++ pps_tools/download.py
@@ -101,6 +101,9 @@ def download_from_drive(filename):
     data['single_top.hdf5']='1UwYbWh6czZ6MTdWW9wTMph-tK_FkttzO'
     data['qcd.hdf5']='1J-97Viw5kKOlVZVHYmSFgZw4P8kJHAkG'
     data['dimuons_100k.hdf5']='13gCId3e815LUBsQvGz-MQHInAp8Fz8n2'
+    data['dimuons_1000_collisions']='1fU8bMs1ZOmTFePkmS_LmYZq9KfaqJG1z'
+    ## BABAR FILES ##
+    data['basicPID_R24-AllEvents-Run1-OnPeak-R24-744.hdf5']='1jpnv5xogtfsk2eIE1C8HGHxYhTCRbC08'
 
     download_file_from_google_drive(data[filename],'data/'+filename)
 
